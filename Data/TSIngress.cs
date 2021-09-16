fc633cbeaca3e3ddb531308dab6f2dff2c8c2a0e
Henrik Milling <hm@vitani.com>
2021-09-16 12:17:18 +0200

    Renamed and new types in model https://confluence.vitani.com/display/OI/VES+konkurrence+-+tidsserie+data+ind+i+Nsight

---
+++ Data/TSIngress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsightModels.Data
+{
+	public class TSIngress
+	{
+        public Guid OrgUUID { get; set; }
+        public Guid SystemID { get; set; }
+        public string MajorID { get; set; }
+        public string MinorID { get; set; }
+        public Guid ValueGroupID { get; set; }
+        public Guid EventID { get; set; }
+        public DateTime MTime { get; set; }
+        public double MValue { get; set; }
+    }
+}
