9193b9f58c2c4591d78d0fae301198b6e80ad80b
Henrik Milling <hm@vitani.com>
2021-11-16 15:46:27 +0100

    Added Keycloak model

---
+++ Keycloak/KCAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NsightModels.Keycloak
+{
+    public class KCAuth
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public int refresh_expires_in { get; set; }
+        public string refresh_token { get; set; }
+        public string token_type { get; set; }
+        public int notBeforePolicy { get; set; }
+        public string session_state { get; set; }
+        public string scope { get; set; }
+    }
+}
