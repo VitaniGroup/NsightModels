8dbe87c98f283553d24dc6a7f9205fbdb4d18f75
Henrik Milling <hm@vitani.com>
2021-12-16 16:15:14 +0100

    Added CustomerID

--- PushOut/Message.cs
+++ PushOut/Message.cs
@@ -8,6 +8,7 @@ namespace NsightModels.PushOut
 {
 	public class Message
     {
+        public string CustomerID { get; set; }
         public string Url { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
